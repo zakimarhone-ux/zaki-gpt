80ad7e37a25138bb17eb579882c618fd0b768ef4
zakimarhone-ux <zakimarhone@gmail.com>
2026-07-27 15:56:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log("App started");
+let projects = JSON.parse(localStorage.getItem("projects")) || [];
+
+function saveProjects() {
+    localStorage.setItem("projects", JSON.stringify(projects));
+}
+
+function drawProjects() {
+
+    let container = document.getElementById("projects");
+
+    container.innerHTML = "";
+
+    projects.forEach((project, index) => {
+
+        container.innerHTML += `
+        <div class="card">
+            <h3>🏗️ ${project.name}</h3>
+
+            <p>💰 الرصيد : ${project.balance} DA</p>
+
+            <p>💸 المصروف : 0 DA</p>
+
+            <p>📅 آخر تحديث : --</p>
+
+        </div>
+        `;
+
+    });
+
+}
+
+function newProject(){
+
+    let name = prompt("اسم الشانطي");
+
+    if(!name) return;
+
+    projects.push({
+        name:name,
+        balance:0
+    });
+
+    saveProjects();
+
+    drawProjects();
+
+}
+
+drawProjects();
